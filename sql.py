495701a626ab4b0b8b6db7f3f710a775de8f0964
majdi <you@example.com>
2022-11-17 14:05:02 +0100

    first commit

--- sql.py
+++ sql.py
@@ -5,19 +5,29 @@ titles  = set()
 
 dic = {}
 with open("Favorite TV Shows - Form Responses 1.csv") as file:
+    # declared the file inside reader variable
     reader  = csv.reader(file)
-    next(reader)
+    # your a choice if you want to work with (list) or (dict)
+    # my work using list
+    next(reader) # if you using (dict) don't should be use this line
+
     for i in reader:
+        # add the column of title from file to titles variable , his type is (set)
+        # using the set means don't repeat any words twice
         titles.add(i[1].upper().strip())
     
     for i in sorted(titles):
+        # declare the words from titles variable in dic anf give the value of key "0"
         dic[i] = 0
 with open("Favorite TV Shows - Form Responses 1.csv") as file:  
     reader = csv.reader(file)
+    # here well be count the words inside the dictionary (dic)
     for row in reader:
         title = row[1].upper().strip()
         if title in dic:
             dic[title] += 1
+
+# print (dic)
 for row in dic:
     print(f"{row} : {dic[row]}")
     
